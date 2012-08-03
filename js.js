6d32162506a84ec27773aa5a9900881624071a04
Dmitry <demx@demx.ru>
2012-08-03 18:51:54 +0400

    Про машины

--- js.js
+++ js.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {
-	$('.title').each(function() {
-		var title = $(this), content = title.next('.content');
+function setSlide(clicker,next)
+{
+	$('.'+clicker).each(function() {
+		var title = $(this), content = title.next('.'+next);
 		title.click(function(){
 			content.slideToggle();
 		});
 	});
+}
+
+$(document).ready(function() {
+	setSlide('title','content');
+	setSlide('question','answer');
+	setSlide('subtitle','code');
 });
